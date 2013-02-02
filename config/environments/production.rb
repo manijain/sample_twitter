8b2177023924cd9bc999dbbb4c218a205e955dfe
manijain <manijain333@gmail.com>
2013-02-02 11:01:38 +0530

    action mailer update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,15 @@ SampleApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => "trysampletwitter.herokuapp.com" }
+
   ActionMailer::Base.smtp_settings = {
    address:'smtp.gmail.com',
    port: 587,
    domain: 'mydomain.com',
    authentication: :plain,
    user_name: "trysampletwitter@gmail.com",
-   password: 'risingsun'
+   password: "risingsun",
+   enable_starttls_auto: true
   }
 end
